648aee3b1ce361bff1b87b7684cbf4984df42b66
Marwanthe0 <marwanthe0@gmail.com>
2024-05-23 22:08:18 +0600

    Add/Update OOP1.cpp

---
+++ 2024 Random/OOP1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+class Student
+{
+public:
+    string name;
+    int roll;
+    char section;
+    double math_marks;
+    int cls;
+    Student(string name, int roll, char section, double math_marks, int cls)
+    {
+        this->name = name;
+        this->roll = roll;
+        this->section = section;
+        this->math_marks = math_marks;
+        this->cls = cls;
+    }
+};
+int main()
+{
+    string nme[3] = {""}
+    return 0;
+}
