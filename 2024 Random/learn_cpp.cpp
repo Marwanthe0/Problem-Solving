6e70a0f911fbe7ed13e664c6fbc383e22a36fa91
Marwanthe0 <marwanthe0@gmail.com>
2024-05-30 16:54:19 +0600

    Add/Update learn_cpp.cpp

---
+++ 2024 Random/learn_cpp.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define M 1000000007
+#define N 1000000
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+}
+int main()
+{
+    marwan();
+    return 0;
+}
