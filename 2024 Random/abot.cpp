dfd8901326abc390a9f9ad829ba20a8ab65fc62e
Marwanthe0 <marwanthe0@gmail.com>
2024-06-13 10:23:28 +0600

    Add/Update abot.cpp

---
+++ 2024 Random/abot.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define M 1000000007
+#define N 1000000
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, m, mx = INT_MIN, row = 0;
+    cin >> n >> m;
+    char grd[n][m];
+    for (int i = 0; i < n; i++)
+    
+        for (int j = 0; j < m; j++)
+        {
+            cin >> grd[i][j];
+            if()
+        }
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
