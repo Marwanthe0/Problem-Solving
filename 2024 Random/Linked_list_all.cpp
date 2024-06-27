8eb803be07d720c21f7f7226b1a8ee4f46e8dae6
Marwanthe0 <marwanthe0@gmail.com>
2024-06-27 20:03:55 +0600

    Add/Update Linked_list_all.cpp

---
+++ 2024 Random/Linked_list_all.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define M 1000000007
+#define N 1000000
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+void marwan()
+{
+    Node *head = new Node(x);
+    while (1)
+    {
+        int x;
+        cin >> x;
+        if (x == -1)
+            break;
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
