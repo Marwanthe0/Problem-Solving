338325de560a48a707d3489e55ab0736ff66485a
Marwanthe0 <marwanthe0@gmail.com>
2024-07-03 19:45:07 +0600

    Add/Update linked_list_prcactice3.cpp

---
+++ 2024 Random/linked_list_prcactice3.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define M 1000000007
+#define N 1000000
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void inserttail(Node *&head, Node *&tail, int data)
+{
+    Node *NewNode = new Node(data);
+    if (head == NULL)
+    {
+        head = tail = NewNode;
+        return;
+    }
+    tail->next = NewNode;
+    NewNode->prev = tail;
+    tail = NewNode;
+}
+bool ispalindrome(Node *&head, Node *&tail)
+{
+    Node *i = head;
+    Node *j = tail;
+    while (i != j)
+    {
+        if (i->data != j->data)
+            return false;
+        if (j == i->next)
+            break;
+        i = i->next;
+        j = j->prev;
+    }
+    return true;
+}
+void printNode(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+void marwan()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    while (1)
+    {
+        int data;
+        cin >> data;
+        if (data == -1)
+            break;
+        inserttail(head, tail, data);
+    }
+    if (ispalindrome(head, tail))
+        yes;
+    else
+        no;
+}
+int main()
+{
+    marwan();
+    return 0;
+}
