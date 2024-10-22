7c4c52dc63ca6f6a3ddfd773579363e3f7d49d94
Marwanthe0 <marwanthe0@gmail.com>
2024-10-22 12:26:53 +0600

    Add/Update abc.java

---
+++ 2024 Random/abc.java
@@ -0,0 +1,18 @@
+// Your First Program
+
+class abc {
+    public static void main(String[] args) {
+        System.out.println("Marwan");
+        System.out.println("Marwan");
+        System.out.println("Marwan");
+        System.out.println("Marwan");
+        System.out.println("Marwan");
+        int x = 5;
+        while (true) {
+            System.out.println(x);
+            x--;
+            if (x <= 0)
+                break;
+        }
+    }
+}
