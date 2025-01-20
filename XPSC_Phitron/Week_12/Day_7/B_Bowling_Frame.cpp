7a3fb3fee00be947342408bed1408cc6fcae1b3d
Marwanthe0 <marwanthe0@gmail.com>
2025-01-21 00:21:47 +0600

    Add/Update B_Bowling_Frame.cpp

---
+++ XPSC_Phitron/Week_12/Day_7/B_Bowling_Frame.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (ll i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    ll n, m;
+    cin >> n >> m;
+    n += m;
+    ll l = 1, r = 6, mid, ans, z;
+    while (l <= r)
+    {
+        mid = l + ((r - l) / 2);
+        z = (mid * (mid + 1)) / 2;
+        if (n < z)
+        {
+            r = mid - 1;
+        }
+        else
+        {
+            ans = mid;
+            l = mid + 1;
+        }
+    }
+    cout << ans << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
