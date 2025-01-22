e6ce6c7163a6bb96e4df7d21cdb5ef32aecde395
Marwanthe0 <marwanthe0@gmail.com>
2025-01-22 18:12:48 +0600

    Add/Update C_Inhabitant_of_the_Deep_Sea.cpp

---
+++ XPSC_Phitron/Week_13/Day_1/C_Inhabitant_of_the_Deep_Sea.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (ll i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    ll l = 0, r = n - 1, count = 0;
+    while (l < r && k > 0)
+    {
+        
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
