e2c2330b488d5d44b6c9fbe6622b99ec9069ccbe
Marwanthe0 <marwanthe0@gmail.com>
2025-02-10 00:25:17 +0600

    Add/Update I_String_Reversal.cpp

---
+++ XPSC_Phitron/Week_15/Day_6/I_String_Reversal.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    string s;
+    cin >> s;
+    int n = s.size();
+    for (int i = 0; i < n / 2; i++)
+    {
+        
+    }
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
