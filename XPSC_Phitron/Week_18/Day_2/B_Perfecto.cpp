a4505517c8cc8381f8b7f8261b4aaf10522659f9
Marwanthe0 <marwanthe0@gmail.com>
2025-03-01 00:01:30 +0600

    Add/Update B_Perfecto.cpp

---
+++ XPSC_Phitron/Week_18/Day_2/B_Perfecto.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+bool pf(int n)
+{
+    int x = sqrtl(n);
+    return (x * x) == n;
+}
+void marwan()
+{
+    int n;
+    cin >> n;
+    if (pf(n * (n + 1) / 2))
+    {
+        cout << -1 << endl;
+        return;
+    }
+    vector<int> v(n);
+    for (int i = 1; i <= n; i++)
+    {
+        v[i - 1] = i;
+    }
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (pf(sum + v[i]))
+        {
+            swap(v[i], v[i + 1]);
+        }
+        sum += v[i];
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
