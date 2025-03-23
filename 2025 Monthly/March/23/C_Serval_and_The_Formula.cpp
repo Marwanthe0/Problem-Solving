6d76ea94f820fefcb5deb9aec43b1d2ae69fde54
Marwanthe0 <marwanthe0@gmail.com>
2025-03-23 23:35:08 +0600

    Add C_Serval_and_The_Formula.cpp

---
+++ 2025 Monthly/March/23/C_Serval_and_The_Formula.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int a, b;
+    cin >> a >> b;
+    for (int i = 0; i <= 1000; i++)
+        if (a + b + (2 * i) == ((a + i) ^ (b + i)))
+            cout << i << endl;
+    cout << endl;
+    cout << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
