bec3b4436626fb01a8da1cf5cdb2987c53eb1270
Marwanthe0 <marwanthe0@gmail.com>
2025-03-26 21:29:19 +0600

    Add Permutation_Construct.cpp

---
+++ 2025 Monthly/March/25/Permutation_Construct.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> kk;
+    vector<vector<int>> pp(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        kk.push_back(i % k);
+        for (int j = 1; j <= n; j++)
+        {
+            if (j != i && j % k == i % k)
+                pp[i].push_back(j);
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << endl;
+        for (auto vl : pp[i])
+            cout << vl << " ";
+        cout << endl;
+    }
+    cout << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
