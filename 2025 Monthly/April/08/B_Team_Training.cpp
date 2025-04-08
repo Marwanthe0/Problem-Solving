f93a52cd72f64e3be64af6cea733102b4ae7483b
Marwanthe0 <marwanthe0@gmail.com>
2025-04-08 18:43:42 +0600

    Add B_Team_Training.cpp

---
+++ 2025 Monthly/April/08/B_Team_Training.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    sort(all(v), greater<int>());
+    int count = 0, cnt = 0, mx = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i] >= k)
+            count++;
+        else
+        {
+            cnt++;
+            mx = min(mx, v[i]);
+            if (mx * cnt >= k)
+                count++, cnt = 0;
+        }
+    }
+    cout << count << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
