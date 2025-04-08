46d1103b806f46ac2982d00787410bd03726f513
Marwanthe0 <marwanthe0@gmail.com>
2025-04-08 21:32:47 +0600

    Add C_Simple_Repetition.cpp

---
+++ 2025 Monthly/April/08/C_Simple_Repetition.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+bool prime(int n)
+{
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+void marwan()
+{
+    int n, k;
+    cin >> n >> k;
+    if (prime(n) && n != 1)
+        yes;
+    else
+        no;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
