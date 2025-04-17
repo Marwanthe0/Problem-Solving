c471c8562ddc444f28a4b33025b9fe68860a601d
Marwanthe0 <marwanthe0@gmail.com>
2025-04-17 17:07:22 +0600

    Add C_Simple_Repetition.cpp

---
+++ 2025 Monthly/April/17/C_Simple_Repetition.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, k;
+    cin >> n >> k;
+    string s = to_string(n), t = s;
+    for (int i = 1; i < k; i++)
+        s += t;
+    if (s.size() > 18)
+    {
+        no;
+        return;
+    }
+    else
+    {
+        if (n == 1)
+        {
+            no;
+            return;
+        }
+        int n = stoull(s);
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                no;
+                return;
+            }
+        }
+        yes;
+    }
+    // cout << s << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
