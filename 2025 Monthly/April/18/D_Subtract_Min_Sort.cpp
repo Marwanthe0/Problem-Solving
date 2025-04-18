6b76a4b197801a399d2b601e6912b16ac6bcc2e0
Marwanthe0 <marwanthe0@gmail.com>
2025-04-18 18:23:09 +0600

    Add D_Subtract_Min_Sort.cpp

---
+++ 2025 Monthly/April/18/D_Subtract_Min_Sort.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    for (int i = 0; i < n - 1; i++)
+    {
+        int t = min(v[i], v[i + 1]);
+        // if (v[i] > v[i + 1])
+        v[i] -= t, v[i + 1] -= t;
+    }
+    if (is_sorted(all(v)))
+        yes;
+    else
+        no;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
