2045a086372892be8bff57bc6da79913044735d5
Marwanthe0 <marwanthe0@gmail.com>
2025-04-19 15:38:01 +0600

    Add L_Two_Colors.cpp

---
+++ 2025 Monthly/April/18/L_Two_Colors.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, k;
+    cin >> k >> n;
+    multiset<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.insert(x);
+    }
+    multiset<int> t = v;
+    for (auto vl : v)
+    {
+        
+    }
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
