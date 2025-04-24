bce9f68ac15e6c220053f5bd4c054c13c146b2ae
Marwanthe0 <marwanthe0@gmail.com>
2025-04-24 22:08:02 +0600

    Add A_Dr_TC.cpp

---
+++ 2025 Monthly/April/24/A_Dr_TC.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int count = 0, ans = 0;
+    for (auto c : s)
+        if (c == '1')
+            count++;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '1')
+            ans += count - 1;
+        else
+            ans += count + 1;
+    }
+    cout << ans << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
