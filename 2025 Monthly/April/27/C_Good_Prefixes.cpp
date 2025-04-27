a0b80ac16545aec371ab44037e44381828e6533a
Marwanthe0 <marwanthe0@gmail.com>
2025-04-27 16:02:59 +0600

    Add C_Good_Prefixes.cpp

---
+++ 2025 Monthly/April/27/C_Good_Prefixes.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    map<int, int> m;
+    int sum = 0, count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        m[v[i]]++;
+        sum += v[i];
+        if ((sum % 2 == 0) && m.find((sum / 2)) != m.end())
+            count++;
+    }
+    cout << count << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
