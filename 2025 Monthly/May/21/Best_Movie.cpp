0708ade1403b1b0abebbe2b693346cd56937e0a6
Marwanthe0 <marwanthe0@gmail.com>
2025-05-22 00:02:08 +0600

    Add Best_Movie.cpp

---
+++ 2025 Monthly/May/21/Best_Movie.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    int mx = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a >= 7)
+            mx = min(mx, b);
+    }
+    if (mx != INT_MAX)
+        cout << mx << endl;
+    else
+        cout << -1 << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
