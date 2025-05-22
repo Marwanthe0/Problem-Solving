f4180f1365eaa91d4503392e2ec1d7ba44535a5e
Marwanthe0 <marwanthe0@gmail.com>
2025-05-22 16:16:22 +0600

    Add Balanced_Lighting.cpp

---
+++ 2025 Monthly/May/21/Balanced_Lighting.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    int a = 0, b = 0, c = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        if (!x)
+            c++;
+        else if (x == 1)
+            a++;
+        else
+            b++;
+    }
+    cerr << a << " " << b << " " << c << endl;
+    while (c--)
+    {
+        if (a >= b)
+            b++;
+        else
+            a++;
+    }
+    if (a == b)
+        yes;
+    else
+        no;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
