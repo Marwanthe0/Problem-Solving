3f40072ff717b2746c5564f5f647566b973b2c90
Marwanthe0 <marwanthe0@gmail.com>
2025-05-25 19:17:18 +0600

    Add D_Final_Price.cpp

---
+++ 2025 Monthly/May/26/D_Final_Price.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    int x;
+    cin >> x;
+    vector<int> v(n - 1);
+    for (auto vl : v)
+    {
+        cin >> vl;
+        x += vl;
+    }
+    cout << x << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
