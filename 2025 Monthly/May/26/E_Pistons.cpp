b4fde16b0bf2137ea67e1f7ae74d0d3d39fac842
Marwanthe0 <marwanthe0@gmail.com>
2025-05-25 20:33:14 +0600

    Add E_Pistons.cpp

---
+++ 2025 Monthly/May/26/E_Pistons.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<pair<int, char>> v(n);
+    int ans = 0;
+    for (auto &[x, y] : v)
+    {
+        cin >> x >> y;
+        
+    }
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
