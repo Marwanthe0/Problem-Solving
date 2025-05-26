af41edd75165868c7c9a5920b6bea09e29845024
Marwanthe0 <marwanthe0@gmail.com>
2025-05-27 00:28:57 +0600

    Add B_Collecting_Game.cpp

---
+++ 2025 Monthly/May/27/B_Collecting_Game.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    vector<int> t, v,a;
+    map<int, int> m;
+    v = t;
+    sort(all(v));
+    int sum = 0;
+    for(auto )
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
