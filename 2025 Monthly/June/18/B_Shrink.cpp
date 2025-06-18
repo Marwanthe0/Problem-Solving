1f290ed8d8f8397e4117575026e50cdef3c42383
Marwanthe0 <marwanthe0@gmail.com>
2025-06-18 15:47:46 +0600

    Add B_Shrink.cpp

---
+++ 2025 Monthly/June/18/B_Shrink.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 1; i <= n; i += 2)
+        v.push_back(i);
+    for (int i = (n & 1 ? n - 1 : n); i >= 2; i -= 2)
+        v.push_back(i);
+    for (auto vl : v)
+        cout << vl << " ";
+    cout << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+};
