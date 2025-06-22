0ab567ba1267c63885eb14d5259e7c44d141a2da
Marwanthe0 <marwanthe0@gmail.com>
2025-06-22 18:31:04 +0600

    Add A_Letter_Home.cpp

---
+++ 2025 Monthly/June/22/A_Letter_Home.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    int mn = *min_element(all(v)), mx = *max_element(all(v));
+    // if (k <= mx && k >= mn)
+    //     cout << mx - mn + min(k - mn, mx - k) << endl;
+    // else
+        cout << mx - mn + min(abs(mn - k), abs(mx - k)) << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
