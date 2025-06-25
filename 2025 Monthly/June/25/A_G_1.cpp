b54a1c2a96993cf590318d16cbbc9b1084ec9824
Marwanthe0 <marwanthe0@gmail.com>
2025-06-25 14:43:21 +0600

    Add A_G_1.cpp

---
+++ 2025 Monthly/June/25/A_G_1.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    int k, count = 0;
+    cin >> k;
+    for (auto vl : v)
+        if (vl >= k)
+            count++;
+    cout << count << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
