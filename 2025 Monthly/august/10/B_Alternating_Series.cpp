69e65f5a0fe83f3b7f174aa90ab3c7333a6544e8
Marwanthe0 <marwanthe0@gmail.com>
2025-08-10 21:03:01 +0600

    Add B_Alternating_Series.cpp

---
+++ 2025 Monthly/august/10/B_Alternating_Series.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < (n & 1 ? n : n - 1); i++)
+    {
+        if (i & 1)
+        {
+            v.push_back(3);
+        }
+        else
+            v.push_back(-1);
+    }
+    if (n % 2 == 0)
+        v.push_back(2);
+    for (auto vl : v)
+        cout << vl << " ";
+    cout << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
