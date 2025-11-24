6b8a1f47fa378d11d5684b9a3bcf3bba29fb0b80
Marwanthe0 <marwanthe0@gmail.com>
2025-11-24 15:09:25 +0600

    Add Range_Xor_Queries.cpp

---
+++ 2025 Monthly/November/24/Range_Xor_Queries.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, q;
+    cin >> n >> q;
+    vector<int> v(n), pf(n + 1, 0);
+    for (auto &vl : v)
+        cin >> vl;
+    for (int i = 1; i < n + 1; i++)
+    {
+        pf[i] = pf[i - 1] ^ v[i - 1];
+    }
+    for (int i = 0; i < q; i++)
+    {
+        int l, r;
+        cin >> l >> r;
+        cout << (pf[r] ^ pf[l - 1]) << endl;
+    }
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
