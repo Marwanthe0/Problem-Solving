cf545987e4fb8e15063a0f37174be3fe6ab647e7
Marwanthe0 <marwanthe0@gmail.com>
2025-11-24 20:12:26 +0600

    Add Array_Manipulation.cpp

---
+++ 2025 Monthly/November/24/Array_Manipulation.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n + 1, 0);
+    for (int i = 0; i < k; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> k;
+        v[a] += k, v[b + 1] -= k;
+    }
+    for (int i = 1; i <= n; i++)
+        v[i] += v[i - 1];
+    // for (auto vl : v)
+    //     cout << vl << " ";
+    // cout << endl;
+    cout << *max_element(all(v)) << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
