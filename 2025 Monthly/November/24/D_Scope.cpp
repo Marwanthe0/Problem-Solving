036a151afaf2e7f9974a0eba809daa254bdadf3d
Marwanthe0 <marwanthe0@gmail.com>
2025-11-24 16:41:50 +0600

    Add D_Scope.cpp

---
+++ 2025 Monthly/November/24/D_Scope.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+void marwan()
+{
+    string s;
+    cin >> s;
+    stack<char> st;
+    map<char, int> m;
+    for (auto c : s)
+    {
+        if (c != ')')
+            st.push(c);
+        else if (c == ')')
+        {
+            while (!st.empty() && st.top() != '(')
+            {
+                m[st.top()]--;
+                st.pop();
+            }
+            if (st.top() == '(')
+                st.pop();
+        }
+        if (m[c])
+        {
+            no;
+            return;
+        }
+        if (c >= 'a' && c <= 'z')
+            m[c]++;
+    }
+    yes;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
