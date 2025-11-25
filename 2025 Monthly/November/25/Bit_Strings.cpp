03ee4989d6bd3549ef8c0ab6325263fdbcd3de44
Marwanthe0 <marwanthe0@gmail.com>
2025-11-25 18:05:14 +0600

    Add Bit_Strings.cpp

---
+++ 2025 Monthly/November/25/Bit_Strings.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+int pw(int a, int b)
+{
+    int x = a;
+    while (--b)
+        x = (x * a) % M;
+    return x;
+}
+void marwan()
+{
+    int n;
+    cin >> n;
+    cout << pw(2, n) << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
