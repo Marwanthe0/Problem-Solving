9b09f6f26e17141ffbd22ebbb74109786cd6ec88
Marwanthe0 <marwanthe0@gmail.com>
2025-11-26 02:21:57 +0600

    Add B_Segment_with_Big_Sum.cpp

---
+++ 2025 Monthly/November/25/B_Segment_with_Big_Sum.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    int l = 0, r = 0, ans = INT_MAX, sum = 0;
+    while (r < n)
+    {
+        sum += v[r];
+        while (sum >= k)
+        {
+            ans = min(ans, r - l + 1);
+            sum -= v[l++];
+        }
+        r++;
+    }
+    if (ans == INT_MAX)
+        cout << -1 << endl;
+    else
+        cout << ans << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
