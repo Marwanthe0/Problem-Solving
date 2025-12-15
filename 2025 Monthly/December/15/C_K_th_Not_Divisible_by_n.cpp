c77353e7d3651af2b2efa212dcf3be0fb30c9844
Marwanthe0 <marwanthe0@gmail.com>
2025-12-15 16:04:09 +0600

    Add C_K_th_Not_Divisible_by_n.cpp

---
+++ 2025 Monthly/December/15/C_K_th_Not_Divisible_by_n.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, k;
+    cin >> n >> k;
+    int x = k / (n - 1);
+    if (k % (n - 1))
+    {
+        cout << (x * n) + (k % (n - 1)) << endl;
+    }
+    else
+        cout << (x * n) - 1 << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
