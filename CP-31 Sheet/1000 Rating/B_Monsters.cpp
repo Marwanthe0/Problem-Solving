cb55abdd03401e931bfbaf6bb3f29a2b1fe65c21
Marwanthe0 <marwanthe0@gmail.com>
2025-12-17 21:11:01 +0600

    Add B_Monsters.cpp

---
+++ CP-31 Sheet/1000 Rating/B_Monsters.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    map<int, vector<int>> m;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        v[i] = (v[i] % (k));
+        m[v[i]].push_back(i + 1);
+    }
+    if (m.find(0) != m.end())
+    {
+        for (auto vl : m[0])
+            cout << vl << " ";
+    }
+    vector<vector<int>> ans;
+    for (auto vl : m)
+    {
+        if (vl.first)
+        {
+            ans.push_back(vl.second);
+        }
+    }
+    for (int i = ans.size() - 1; i >= 0; i--)
+    {
+        for(auto vl:ans[i])
+            cout << vl << " ";
+    }
+    cout << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
