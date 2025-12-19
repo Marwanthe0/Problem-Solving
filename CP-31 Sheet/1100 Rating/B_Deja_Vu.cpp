93bc84c79391c72a0755e73ad1a08f6b27aa409f
Marwanthe0 <marwanthe0@gmail.com>
2025-12-20 00:35:57 +0600

    Add B_Deja_Vu.cpp

---
+++ CP-31 Sheet/1100 Rating/B_Deja_Vu.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, q;
+    cin >> n >> q;
+    vector<int> v(n), x(q);
+    for (auto &vl : v)
+        cin >> vl;
+    for (auto &vl : x)
+        cin >> vl;
+    vector<int> xs;
+    int mn = INT_MAX;
+    for (auto vl : x)
+    {
+        if (vl < mn)
+            xs.push_back(vl);
+        mn = min(mn, vl);
+    }
+    x = xs;
+    // for (auto vl : x)
+    //     cerr << vl << " ";
+    // cerr << endl;
+    for (auto &vl : v)
+    {
+        for (auto op : x)
+        {
+            if (vl % (1 << op) == 0)
+                vl += (1 << (op - 1));
+        }
+    }
+    for (auto vl : v)
+        cout << vl << " ";
+    cout << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
