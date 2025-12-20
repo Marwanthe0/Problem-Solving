893c66b8ada4c313b0ad7b1a7b62915942cef41a
Marwanthe0 <marwanthe0@gmail.com>
2025-12-21 00:30:56 +0600

    Add E_Negatives_and_Positives.cpp

---
+++ CP-31 Sheet/1100 Rating/E_Negatives_and_Positives.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    int sum = 0, mn = INT_MAX, neg = 0;
+    for (auto &vl : v)
+    {
+        cin >> vl;
+        if (vl < 0)
+            neg++;
+        sum += abs(vl);
+        mn = min(mn, abs(vl));
+    }
+    if (neg & 1)
+        cout << sum - (2 * mn) << endl;
+    else
+        cout << sum << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
