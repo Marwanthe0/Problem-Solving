a58fa8bb532c0785c70a41adeceb93db5931fd0b
Marwanthe0 <marwanthe0@gmail.com>
2025-12-20 19:40:58 +0600

    Add B_Lunatic_Never_Content.cpp

---
+++ CP-31 Sheet/1100 Rating/B_Lunatic_Never_Content.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    int x = 0;
+    for (int i = 0; i < n / 2; i++)
+    {
+        int a = v[i], b = v[n - 1 - i];
+        if (x)
+        {
+            x = __gcd(x, abs(a - b));
+        }
+        else
+            x = abs(a - b);
+    }
+    cout << x << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
