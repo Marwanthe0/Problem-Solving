b793ffd9c4b560ad71cb54810e3dcb7321cea6ba
Marwanthe0 <marwanthe0@gmail.com>
2025-12-22 00:56:16 +0600

    Add B_Erase_First_or_Second_Letter.cpp

---
+++ CP-31 Sheet/1100 Rating/B_Erase_First_or_Second_Letter.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int ans = 0, count = 1;
+    set<char> st;
+    int x = 0;
+    for (int i = n; i >= 1; i--)
+    {
+        st.insert(s[x++]);
+        ans += st.size();
+    }
+    // cerr << endl;
+    cout << ans << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
