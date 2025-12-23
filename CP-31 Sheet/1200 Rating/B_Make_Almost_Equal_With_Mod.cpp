1d7c773b8a7ebf7cca8604546dab10434a823c42
Marwanthe0 <marwanthe0@gmail.com>
2025-12-23 15:54:07 +0600

    Add B_Make_Almost_Equal_With_Mod.cpp

---
+++ CP-31 Sheet/1200 Rating/B_Make_Almost_Equal_With_Mod.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    for (int i = 1; i < 64; i++)
+    {
+        int x = (1ll << i);
+        set<int> st;
+        for (auto vl : v)
+            st.insert(vl % x);
+        if (st.size() == 2)
+        {
+            cout << x << endl;
+            return;
+        }
+    }
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
