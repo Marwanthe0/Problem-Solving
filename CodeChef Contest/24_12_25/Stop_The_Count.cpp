ead7297fb09a12b627e36ce189d49c4c9802101e
Marwanthe0 <marwanthe0@gmail.com>
2025-12-24 20:36:59 +0600

    Add Stop_The_Count.cpp

---
+++ CodeChef Contest/24_12_25/Stop_The_Count.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int ans = 0, count = 0, tt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '1')
+            count++;
+        else
+            tt++;
+        if (count > tt)
+            ans++;
+    }
+    cout << ans << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
