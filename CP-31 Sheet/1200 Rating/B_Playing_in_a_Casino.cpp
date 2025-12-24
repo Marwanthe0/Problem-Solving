0fa3e2a3e251a0d14c651e047097d8a09f0db487
Marwanthe0 <marwanthe0@gmail.com>
2025-12-25 01:32:13 +0600

    Add B_Playing_in_a_Casino.cpp

---
+++ CP-31 Sheet/1200 Rating/B_Playing_in_a_Casino.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> v(m, vector<int>(n, 0));
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cin >> v[i][j];
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i < m; i++)
+    {
+        sort(all(v[i]));
+        int sum = 0;
+        for (int j = 0; j < n; j++)
+        {
+            int left = (n - j) - 1, right = j;
+            sum += (right - left) * v[i][j];
+        }
+        ans += sum;
+    }
+    cout << ans << endl;
+    // for (int i = 0; i < m; i++)
+    // {
+    //     // sort(all(v[i]));
+    //     for (int j = 0; j < n; j++)
+    //         cout << v[j][i] << " ";
+    //     cout << endl;
+    // }
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
