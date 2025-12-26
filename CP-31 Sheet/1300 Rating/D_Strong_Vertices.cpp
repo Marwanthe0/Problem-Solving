bac6c6fe2f2ec0befbe8163889494d962131ef92
Marwanthe0 <marwanthe0@gmail.com>
2025-12-27 01:37:15 +0600

    Add D_Strong_Vertices.cpp

---
+++ CP-31 Sheet/1300 Rating/D_Strong_Vertices.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (auto &vl : a)
+        cin >> vl;
+    for (auto &vl : b)
+        cin >> vl;
+    vector<int> dif(n);
+    map<int, vector<int>> m;
+    for (int i = 0; i < n; i++)
+    {
+        dif[i] = a[i] - b[i];
+        m[dif[i]].push_back(i + 1);
+    }
+    vector<int> ans = (--m.end())->second;
+    cout << ans.size() << endl;
+    for (auto vl : ans)
+        cout << vl << " ";
+    cout << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
