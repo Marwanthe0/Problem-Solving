ca99330c05d6695f74929e9c3845ed4ac044074f
Marwanthe0 <marwanthe0@gmail.com>
2025-12-28 17:18:08 +0600

    Add D_Buying_Shovels.cpp

---
+++ CP-31 Sheet/1300 Rating/D_Buying_Shovels.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, k;
+    cin >> n >> k;
+    if (k >= n)
+    {
+        cout << 1 << endl;
+        return;
+    }
+    int ans = n;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (i > k)
+            break;
+        if ((n % i) == 0)
+        {
+            if ((n / i) != i && (n / i) <= k)
+            {
+                ans = min(ans, i);
+            }
+            ans = min(ans, n / i);
+        }
+    }
+    cout << ans << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
