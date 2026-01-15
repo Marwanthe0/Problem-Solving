03ee0b35fa4086352358e49ef5a65d6df0128cef
Marwanthe0 <marwanthe0@gmail.com>
2026-01-15 19:52:44 +0600

    Add F_Greetings.cpp

---
+++ CP-31 Sheet/1500 Rating/F_Greetings.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    vector<pair<int, int>> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].first >> v[i].second;
+    }
+    sort(all(v), greater<pair<int, int>>());
+    // pbds<int> pb;
+    pbds<int> pb;
+    int ans = 0;
+    for (auto vl : v)
+    {
+        int y = vl.second;
+        ans += pb.order_of_key(y);
+        pb.insert(y);
+    }
+    cout << ans << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
