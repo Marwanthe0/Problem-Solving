74f3a9e7107d0fc33f3438e8593678e4d4851956
Marwanthe0 <marwanthe0@gmail.com>
2026-01-15 23:37:42 +0600

    Add E_Data_Structures_Fan.cpp

---
+++ CP-31 Sheet/1500 Rating/E_Data_Structures_Fan.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n), px(n + 1, 0), po(n + 1, 0);
+    for (auto &vl : v)
+        cin >> vl;
+    string s;
+    cin >> s;
+    for (int i = 1; i <= n; i++)
+    {
+        px[i] = px[i - 1] ^ ((s[i - 1] == '0') ? v[i - 1] : 0);
+        po[i] = po[i - 1] ^ ((s[i - 1] == '1') ? v[i - 1] : 0);
+    }
+    // for (auto vl : px)
+    //     cerr << vl << " ";
+    // cerr << endl;
+    // for (auto vl : po)
+    //     cerr << vl << " ";
+    // cerr << endl;
+    // cerr << endl;
+    int x = px.back(), y = po.back();
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int op;
+        cin >> op;
+        if (op == 1)
+        {
+            int l, r;
+            cin >> l >> r;
+            x ^= ((px[r] ^ px[l - 1]) ^ (po[r] ^ po[l - 1]));
+            y ^= ((px[r] ^ px[l - 1]) ^ (po[r] ^ po[l - 1]));
+        }
+        else
+        {
+            int t;
+            cin >> t;
+            if (t == 0)
+                cout << x << " ";
+            else
+                cout << y << " ";
+        }
+    }
+    cout << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
