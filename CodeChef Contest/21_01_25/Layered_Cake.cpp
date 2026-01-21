bcd5d28cf1fc5d461509807e3aa0fd186d297799
Marwanthe0 <marwanthe0@gmail.com>
2026-01-21 20:38:22 +0600

    Add Layered_Cake.cpp

---
+++ CodeChef Contest/21_01_25/Layered_Cake.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> v(n), a(m);
+    for (auto &vl : v)
+        cin >> vl;
+    for (auto &vl : a)
+        cin >> vl;
+    sort(all(a));
+    sort(all(v));
+    int ans = 0, j = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < m; j++)
+        {
+            if (a[j] < v[i])
+                count++;
+            else
+                break;
+        }
+        ans += count;
+    }
+    cout << ans << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
