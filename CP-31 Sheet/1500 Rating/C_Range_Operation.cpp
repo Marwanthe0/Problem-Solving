028a77c026ddeefd4e80fe94c50db5791841a1df
Marwanthe0 <marwanthe0@gmail.com>
2026-02-01 01:25:37 +0600

    Add C_Range_Operation.cpp

---
+++ CP-31 Sheet/1500 Rating/C_Range_Operation.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << "-1" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> v[i];
+    }
+    int cur = 0, best = 0, sum = accumulate(all(v), 0ll);
+    for (int i = 1; i <= n; i++)
+    {
+        int gain = 2 * i - v[i];
+        cur = max(gain, cur + gain);
+        best = max(best, cur);
+        // cerr << cur << " " << best << " " << endl;
+    }
+    // cerr << endl;
+    cout << sum + best << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
