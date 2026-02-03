3fd9b4e5318c95721166fa4d4281e9d5e175ea8d
Marwanthe0 <marwanthe0@gmail.com>
2026-02-04 02:54:05 +0600

    Add B_Yet_Another_MEX_Problem.cpp

---
+++ UPSOLVE/B_Yet_Another_MEX_Problem.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << "-1" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    set<int> s;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        s.insert(x);
+    }
+    for (int i = 0; i < k - 1; i++)
+    {
+        if (!s.count(i))
+        {
+            cout << i << endl;
+            return;
+        }
+    }
+    cout << k - 1 << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
