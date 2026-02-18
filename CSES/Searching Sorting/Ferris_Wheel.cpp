815929cf9a3052306132c6903996776134ed2217
Marwanthe0 <marwanthe0@gmail.com>
2026-02-18 16:12:48 +0600

    Add Ferris_Wheel.cpp

---
+++ CSES/Searching Sorting/Ferris_Wheel.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    sort(all(v));
+    int l = 0, r = n - 1, count = 0;
+    while (l <= r)
+    {
+        if (v[l] + v[r] <= k)
+            l++, r--, count++;
+        else
+            r--, count++;
+    }
+    cout << count << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
