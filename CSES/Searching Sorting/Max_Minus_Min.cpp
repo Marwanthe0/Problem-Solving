712d159eb05e06dbf1efb0ca92be4d10c660280c
Marwanthe0 <marwanthe0@gmail.com>
2026-02-19 00:32:31 +0600

    Add Max_Minus_Min.cpp

---
+++ CSES/Searching Sorting/Max_Minus_Min.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << "-1" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan()
+{
+    int n;
+    cin >> n;
+    set<int> s;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        s.insert(x);
+    }
+    int ans = *(--s.end()) - *s.begin();
+    while (*(--s.end()) < 1e9)
+    {
+        int x = *s.begin();
+        s.erase(s.begin());
+        s.insert(x << 1);
+        ans = min(ans, *(--s.end()) - *s.begin());
+    }
+    cout << ans << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
