41fcd10f3f49c2888749f0ddd5e5248ba18fd10a
Marwanthe0 <marwanthe0@gmail.com>
2026-02-19 03:57:16 +0600

    Add Factory_Machines.cpp

---
+++ CSES/Searching Sorting/Factory_Machines.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    int l = 1, h = LONG_MAX, ans = LONG_MAX;
+    auto ok = [&](int m)
+    {
+        int count = 0ll;
+        for (auto vl : v)
+        {
+            count += m / vl;
+            if (count >= k)
+                return true;
+        }
+        return count >= k;
+    };
+    while (l <= h)
+    {
+        int m = l + (h - l) / 2;
+        if (ok(m))
+        {
+            h = m - 1;
+            ans = m;
+        }
+        else
+            l = m + 1;
+    }
+    cout << ans << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
