e9595220d3666d07710f204c2c5ee727e910af97
Marwanthe0 <marwanthe0@gmail.com>
2026-02-18 18:45:05 +0600

    Add Maximum_Subarray_Sum.cpp

---
+++ CSES/Searching Sorting/Maximum_Subarray_Sum.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    int mn = 0, ans = INT_MIN, sum = 0;
+    for (auto &vl : v)
+    {
+        cin >> vl;
+        sum += vl;
+        ans = max(ans, sum - mn);
+        mn = min(mn, sum);
+    }
+    cout << ans << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
