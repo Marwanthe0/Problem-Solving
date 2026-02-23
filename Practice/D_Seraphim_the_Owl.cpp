fe5a326d2d10519027f710a7b3b44ea09710f916
Marwanthe0 <marwanthe0@gmail.com>
2026-02-24 01:57:15 +0600

    Add D_Seraphim_the_Owl.cpp

---
+++ Practice/D_Seraphim_the_Owl.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << "-1" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n), b(n), v(n);
+    for (auto &vl : a)
+        cin >> vl;
+    for (auto &vl : b)
+        cin >> vl;
+    int sum = 0;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (a[i] < b[i])
+        {
+            v[i] = sum + a[i];
+            sum = v[i];
+        }
+        else
+        {
+            v[i] = sum + a[i];
+            sum += b[i];
+        }
+    }
+    cout << *min_element(v.begin(), v.begin() + k) << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
