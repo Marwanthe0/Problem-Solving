6fbe312069ce9c2d686fad453216df0bb2266ba2
Marwanthe0 <marwanthe0@gmail.com>
2026-02-24 23:58:50 +0600

    Add Number_Spiral.cpp

---
+++ CSES/Introductory Problems/Number_Spiral.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << "-1" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan()
+{
+    int x, y;
+    cin >> x >> y;
+    if (x <= y)
+    {
+        if (y & 1)
+        {
+            cout << (y * 1ll * y) - (x - 1ll) << endl;
+        }
+        else
+            cout << ((y - 1) * 1ll * (y - 1)) + x << endl;
+    }
+    else
+    {
+        if (x % 2 == 0)
+        {
+            cout << (x * 1ll * x) - (y - 1) << endl;
+        }
+        else
+            cout << ((x - 1) * 1ll * (x - 1)) + y << endl;
+    }
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
+}
