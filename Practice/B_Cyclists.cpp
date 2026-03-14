80c42cdccf8559c7561f0eafb39dc2fad41d96ea
Marwanthe0 <marwanthe0@gmail.com>
2026-03-14 22:55:43 +0600

    Add B_Cyclists.cpp

---
+++ Practice/B_Cyclists.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << "-1" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int n, k, p, m;
+    cin >> n >> k >> p >> m;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    v[p - 1] *= -1ll;
+    // for (auto vl : v)
+    //     cerr << vl << ' ';
+    // cerr << endl;
+    int ans = 0;
+    while (m > 0) {
+        int mn = min_element(v.begin(), v.begin() + k) - v.begin();
+        if (v[mn] < 0) {
+            if (v[mn] * -1 > m)
+                break;
+            else
+                ans++, m += v[mn];
+        } else {
+            if (v[mn] > m)
+                break;
+            else
+                m -= v[mn];
+        }
+        v.push_back(v[mn]);
+        v.erase(v.begin() + mn);
+    }
+    // cerr << endl;
+    cout << ans << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
