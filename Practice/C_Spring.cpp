9fa105f7ebd4148698334f5d54ebaf080843d5cd
Marwanthe0 <marwanthe0@gmail.com>
2026-03-16 23:16:14 +0600

    Add C_Spring.cpp

---
+++ Practice/C_Spring.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << "-1" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+int lcm(int a, int b) {
+    return (a * 1ll * b) / __gcd(a, b);
+}
+void marwan() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    int ab = d / lcm(a, b), ac = d / lcm(a, c), bc = d / lcm(b, c), abc = d / lcm(a, lcm(b, c)), oa = d / a, ob = d / b, oc = d / c;
+    oa = oa - ab - ac + abc, ob = ob - ab - bc + abc, oc = oc - ac - bc + abc;
+    // cout << oa << " " << ob << " " << oc << endl;
+    // cout << ab << " " << bc << " " << ac << " " << abc << endl;
+    ab -= abc, bc -= abc, ac -= abc;
+    cout << (6 * oa + 3 * (ab + ac) + 2 * abc) << " " << (6 * ob + 3 * (ab + bc) + 2 * abc) << " " << (6 * oc + 3 * (bc + ac) + 2 * abc) << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
