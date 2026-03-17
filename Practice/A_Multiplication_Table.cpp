cd7930447994a5158bfe81e87bf66076ed7ead72
Marwanthe0 <marwanthe0@gmail.com>
2026-03-18 03:22:30 +0600

    Add A_Multiplication_Table.cpp

---
+++ Practice/A_Multiplication_Table.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan() {
+    int n, m;
+    cin >> n >> m;
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        if (m >= i && m <= (i * n) && (m % i) == 0)
+            count++;
+    }
+    cout << count << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
