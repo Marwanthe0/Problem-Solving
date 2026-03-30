ef732d8c34d15eed8886f4ceac8bd0a94a7cf824
Marwanthe0 <marwanthe0@gmail.com>
2026-03-31 02:45:51 +0600

    Add C_Roads_in_Laurasia.cpp

---
+++ Virtual Contest/IUT Eid Salami Contest/C_Roads_in_Laurasia.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << "-1" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int n;
+    cin >> n;
+    vector<vector<int>> g(n + 1);
+    for (int i = 1; i < n; i++) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        if (g[i].size() == 1)
+            count++;
+    }
+    cout << (count / 2) + (count % 2 != 0) << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
