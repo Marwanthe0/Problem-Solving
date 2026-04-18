1d7d9ff321dd34f00a31b70982e5da7a576aab5c
Marwanthe0 <marwanthe0@gmail.com>
2026-04-18 23:53:55 +0600

    Add K_Time_Display_Stickers.cpp

---
+++ CP-31 Sheet/1600 Rating/K_Time_Display_Stickers.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << "-1" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    map<int, int> m;
+    for (auto c : s)
+        m[c - '0']++;
+    int x = m[0];
+    int y = INT_MIN;
+    for (int i = 1; i < 9; i++) {
+        y = max(y, m[i]);
+    }
+    x = min(x, y);
+    
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
