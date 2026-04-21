8c1ee6c5e2013089c1a8d90eb8b853ce74e2b8fb
Marwanthe0 <marwanthe0@gmail.com>
2026-04-21 20:17:21 +0600

    Add D_1_Max_Sum_OR_Easy_Version.cpp

---
+++ CP-31 Sheet/1600 Rating/D_1_Max_Sum_OR_Easy_Version.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << "-1" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int l, r;
+    cin >> l >> r;
+    vector<int> ans(r + 1);
+    unordered_set<int> s;
+    for (int i = 0; i <= r; i++) {
+        ans[i] = -1;
+        s.insert(i);
+    }
+    int mx = __lg(r), last = 0;
+    for (int i = r; i >= l; i--) {
+        int x = 0;
+        if (ans[i] != -1) {
+            last += ans[i] | i;
+            continue;
+        }
+        for (int j = 0; j <= mx; j++) {
+            if (!(1 & (i >> j))) {
+                x = x | (1 << j);
+            }
+        }
+        while (!s.count(x)) {
+            x = x & ~(1 << __lg(x));
+        }
+        if (s.count(x)) {
+            ans[i] = x;
+            ans[x] = i;
+            s.erase(i);
+            s.erase(x);
+        }
+        last += (i | x);
+    }
+    cout << last << endl;
+    for (auto vl : ans)
+        cout << vl << " ";
+    cout << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
