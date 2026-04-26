33e0ef01f8deec77d4883edf1d8809bd11864e7e
Marwanthe0 <marwanthe0@gmail.com>
2026-04-26 21:39:55 +0600

    Add A_Balls.cpp

---
+++ CodeForces Contests/Div_2 (25.04.26)/A_Balls.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+int f(int i,int last,)
+void marwan() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    int ans = f(N - 1, 0, v);
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
