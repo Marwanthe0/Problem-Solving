26e380e4d065e334990dc3bc900597cf4579aaa3
Marwanthe0 <marwanthe0@gmail.com>
2026-04-29 21:00:05 +0600

    Add Palindrome_Check.cpp

---
+++ CodeChef Contest/29_04_26/Palindrome_Check.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define minus cout << "-1" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> v;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if (x != k)
+            v.push_back(x);
+    }
+    int l = 0, r = v.size() - 1;
+    while (l <= r) {
+        if (v[l] != v[r]) {
+            no;
+            return;
+        }
+        l++, r--;
+    }
+    yes;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
