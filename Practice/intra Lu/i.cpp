198cc04eb530479744e5843b53705eb6cd932d00
Marwanthe0 <marwanthe0@gmail.com>
2026-05-02 02:02:36 +0600

    Add i.cpp

---
+++ Practice/intra Lu/i.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+void marwan()
+{k
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    while (k--) {
+        int l, r;
+        cin >> l >> r;
+    }
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
