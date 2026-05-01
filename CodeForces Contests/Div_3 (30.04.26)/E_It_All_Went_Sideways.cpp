fbd3a81756adfd839eb0a308b64adf7c540bd693
Marwanthe0 <marwanthe0@gmail.com>
2026-05-01 20:12:36 +0600

    Add E_It_All_Went_Sideways.cpp

---
+++ CodeForces Contests/Div_3 (30.04.26)/E_It_All_Went_Sideways.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << "-1" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+
+void marwan() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    vector<int> sf(n);
+    sf[n - 1] = v[n - 1];
+    for (int i = n - 2; i >= 0; i--) {
+        sf[i] = min(v[i], sf[i + 1]);
+    }
+    int count = 0;
+    int mn = 0;
+    for (int i = 0; i < n; i++) {
+        count += v[i];
+        mn += sf[i];
+    }
+    int moves = count - mn;
+    count = 0;
+    int more = 0;
+    for (int i = n - 2; i >= 0; i--) {
+        if (sf[i] != sf[i + 1]) {
+            more = max(more, count);
+            count = 0;
+        } else
+            count++;
+    }
+    more = max(more, count);
+    cout << more + moves << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
