867b3c9b102412315971bd481904049b55b1ae74
Marwanthe0 <marwanthe0@gmail.com>
2026-05-13 22:31:14 +0600

    Add Unfazed_By_All_Shades.cpp

---
+++ CodeChef Contest/13_05_26/Unfazed_By_All_Shades.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << "-1" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int n, k;
+    cin >> n >> k;
+    vector<vector<int>> g(n + 1);
+    vector<int> vis(n + 1, 0),level;
+    for (int i = 0; i + 1 < n; i++) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
