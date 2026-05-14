a888bf415abebf14c8d155bfa889606241aa91f4
Marwanthe0 <marwanthe0@gmail.com>
2026-05-15 01:04:52 +0600

    Add Slimes.cpp

---
+++ AtCoder Contest/Educational DP/Slimes.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+int n;
+vector<int> v;
+int f(int i, int j) {
+
+    int nimu = f(i + 2,)
+}
+void marwan() {
+    cin >> n;
+    v.resize(n);
+    for (auto &vl : v)
+        cin >> vl;
+    int ans = f(0, 1);
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
