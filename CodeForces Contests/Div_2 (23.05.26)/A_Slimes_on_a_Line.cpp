33cb2d02f31a1fc89c52a567bf1db087ca2cc86f
Marwanthe0 <marwanthe0@gmail.com>
2026-05-23 20:38:57 +0600

    Add/Update A_Slimes_on_a_Line.cpp

---
+++ CodeForces Contests/Div_2 (23.05.26)/A_Slimes_on_a_Line.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << "-1" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    sort(all(v));
+    int mx = *max_element(all(v));
+    int mn = *min_element(all(v)), ans = INT_MAX;
+    for (int i = 1; i <= mx; i++) {
+        ans = min(ans, max(abs(i - mn), abs(i - mx)));
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
