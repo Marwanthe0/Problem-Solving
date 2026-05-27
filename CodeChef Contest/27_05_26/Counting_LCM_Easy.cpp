87369ab3407ca9e70f4429dbba57f20791aca6a0
Marwanthe0 <marwanthe0@gmail.com>
2026-05-27 22:27:08 +0600

    Add/Update Counting_LCM_Easy.cpp

---
+++ CodeChef Contest/27_05_26/Counting_LCM_Easy.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000000
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define forn for (int i = 0; i < n; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << "-1" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int z;
+    cin >> z;
+    int ans = 0ll, prop = 0;
+    for (int i = 1; i <= z; i++) {
+        int x = i;
+        int val = max(x, z / x);
+        if (z % x)
+            val++;
+        if (val * x == z)
+            val++;
+        for (int y = 1; y <= z; y++) {
+            cerr << x << " " << y << " " << z << "::";
+            if (y / __gcd(x, y) > z / x) {
+                ans += 2;
+                prop++;
+                cerr << 1 << endl;
+            } else {
+                cerr << 0 << endl;
+            }
+        }
+        cerr << endl;
+    }
+    cerr << endl;
+    cerr << endl;
+    cout << ans << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
