0126122185d9b291614bfdf3b828242daeddd520
Marwanthe0 <marwanthe0@gmail.com>
2026-06-07 18:53:55 +0600

    Add/Update abc461a.cpp

---
+++ AtCoder Contest/ABC 06.07.26/abc461a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int a, d;
+    cin >> a >> d;
+    if (a <= d)
+        yes;
+    else
+        no;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
