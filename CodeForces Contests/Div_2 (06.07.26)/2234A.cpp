7eebca48ddd7b9b9ab6ebbe3f13561d8e29e2517
Marwanthe0 <marwanthe0@gmail.com>
2026-06-07 20:37:59 +0600

    Add/Update 2234A.cpp

---
+++ CodeForces Contests/Div_2 (06.07.26)/2234A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    sort(all(v), greater<int>());
+    for (int i = 2; i < n; i++) {
+        if (v[i] != v[i - 2] % v[i - 1]) {
+            minus;
+            return;
+        }
+    }
+    cout << v[0] << " " << v[1] << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
