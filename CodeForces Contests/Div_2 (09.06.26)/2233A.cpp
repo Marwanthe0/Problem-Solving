fb322d6950a9121257314c0c244b66e92691a8b5
Marwanthe0 <marwanthe0@gmail.com>
2026-06-09 20:46:17 +0600

    Add/Update 2233A.cpp

---
+++ CodeForces Contests/Div_2 (09.06.26)/2233A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int n, t, nk, s;
+    cin >> n >> t >> nk >> s;
+    int ans = (n / (t + nk)) + (n % (t + nk) != 0);
+    int sp = 10 * nk;
+    n -= s * t;
+    ans = min(ans, s + (n / (t + sp) + (n % (t + sp) != 0)));
+    cout << ans << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
