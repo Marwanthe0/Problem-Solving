c7411d60c7f0879276d158703ec93e3e83cc9b8e
Marwanthe0 <marwanthe0@gmail.com>
2026-06-28 20:14:27 +0600

    Add/Update C_Dreaming_of_Freedom.cpp

---
+++ CP-31 Sheet/1700 Rating/C_Dreaming_of_Freedom.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int n, k;
+    cin >> n >> k;
+    if ((n % 2 == 0 && k > 1) || (n % k == 0) || (k >= n && n > 1))
+        no;
+    else
+        yes;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
