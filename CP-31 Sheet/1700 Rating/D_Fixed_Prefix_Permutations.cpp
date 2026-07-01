77bb8125d667a9c0c6dcb42e8f2165199290b35d
Marwanthe0 <marwanthe0@gmail.com>
2026-07-02 02:06:15 +0600

    Add/Update D_Fixed_Prefix_Permutations.cpp

---
+++ CP-31 Sheet/1700 Rating/D_Fixed_Prefix_Permutations.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> v(n, vector<int>(m + 1));
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> v[i][j];
+        }
+    }
+    
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
