6ba501b85b0cf6bf556c1cccad662474626608e6
Marwanthe0 <marwanthe0@gmail.com>
2026-07-01 20:38:17 +0600

    Add/Update Equivalent_Exchange.cpp

---
+++ CodeChef Contest/01_07_26/Equivalent_Exchange.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    auto ok = [&](int x) {
+        int r = x, b = k - x;
+        bool flag = true;
+        for (auto vl : v) {
+            r += vl;
+            b -= vl;
+            // cerr << r << " " << b << endl;
+            if (r < 0 || b < 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    };
+    for (int x = 0; x <= k; x++) {
+        // cerr << x << endl;
+        if (ok(x)) {
+            yes;
+            return;
+        }
+        // cerr << endl;
+    }
+    no;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
