1e27052c2b047e738fb40dedb88998165038b3c7
Marwanthe0 <marwanthe0@gmail.com>
2026-07-06 20:38:39 +0600

    Add/Update A_Bigrams.cpp

---
+++ CodeForces Contests/06_07_26/A_Bigrams.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    bool flag = false;
+    int count = 0;
+    for (auto &vl : v) {
+        cin >> vl;
+        if (vl >= 3)
+            flag = true;
+        else if (vl >= 2)
+            count++;
+    }
+    if (count >= 2)
+        flag = true;
+    if (flag)
+        yes;
+    else
+        no;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
