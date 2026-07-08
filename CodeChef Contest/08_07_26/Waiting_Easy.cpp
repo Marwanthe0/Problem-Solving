5133995a39c87d7debb347a263dca676e4dcb42c
Marwanthe0 <marwanthe0@gmail.com>
2026-07-08 20:54:16 +0600

    Add/Update Waiting_Easy.cpp

---
+++ CodeChef Contest/08_07_26/Waiting_Easy.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &vl : v)
+        cin >> vl;
+    int mn = 0, ans = 0ll;
+    for (int i = 0; i < n; i++) {
+        if (mn > v[i])
+            ans += mn - v[i];
+        mn = max(mn, v[i]);
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
