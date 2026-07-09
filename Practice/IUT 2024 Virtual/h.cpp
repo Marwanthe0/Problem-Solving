034c0e7cb9c4068cd6632cc415104092d93b8078
Marwanthe0 <marwanthe0@gmail.com>
2026-07-09 19:47:32 +0600

    Add/Update h.cpp

---
+++ Practice/IUT 2024 Virtual/h.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    string s;
+    cin >> s;
+    auto f = [&](char c) {
+        if (c == 'z')
+            return 'a';
+        else if (c == 'Z')
+            return 'A';
+        else
+            return ++c;
+    };
+    for (int i = 0; i < s.size(); i++) {
+        cout << f(s[i]);
+    }
+    cout << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
