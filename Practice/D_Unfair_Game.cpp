3abe6dc0a781e0bd32d5e677d9d9a9fb6d52ac55
Marwanthe0 <marwanthe0@gmail.com>
2026-07-11 03:08:55 +0600

    Add/Update D_Unfair_Game.cpp

---
+++ Practice/D_Unfair_Game.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+vector<int> fact(N + 5, 0), ifact(N + 5, 0);
+int mod(int x, int m = M) { return x % m; }
+int binexp(int a, int b) {
+    int ans = 1ll;
+    while (b) {
+        if (b & 1)
+            ans = mod(ans * 1ll * a);
+        a = mod(a * 1ll * a);
+        b >>= 1;
+    }
+    return mod(ans);
+}
+void pre() {
+    fact[0] = 1ll;
+    for (int i = 1; i <= N; i++)
+        fact[i] = mod(fact[i - 1] * 1ll * i);
+    ifact[N] = binexp(fact[N], M - 2);
+    for (int i = N; i > 0; i--) {
+        ifact[i - 1] = mod(ifact[i] * 1ll * i);
+    }
+}
+int nCr(int n, int r) {
+    if (r == 0 || n == r)
+        return 1ll;
+    if (n < r || r < 0 || n <= 0)
+        return 0ll;
+    return mod(mod(fact[n] * 1ll * ifact[r]) * 1ll * ifact[n - r]);
+}
+void marwan() {
+    int n, k, ans = 0ll;
+    cin >> n >> k;
+    for (int i = 1; i < n; i *= 2) {
+        int x = __lg(i);
+        for (int j = 0; j <= x && j + x < k; j++)
+            ans += nCr(x, j);
+    }
+    if (__lg(n) + 1 <= k)
+        ans++;
+    cout << n - ans << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    pre();
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
