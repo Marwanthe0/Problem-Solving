7279e867652449ac093642edd3b986c418463600
Marwanthe0 <marwanthe0@gmail.com>
2026-08-05 20:31:34 +0600

    Add/Update Subscription_Cost.cpp

---
+++ CodeChef Contest/05_08_25/Subscription_Cost.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int n, x, y;
+    cin >> n >> x >> y;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (i < 3) {
+            ans += x;
+        } else
+            ans += y;
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
