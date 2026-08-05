5392d0e448c541295204b8abd707f85d0b1d1ced
Marwanthe0 <marwanthe0@gmail.com>
2026-08-05 20:38:48 +0600

    Add/Update Bishop_Moves.cpp

---
+++ CodeChef Contest/05_08_25/Bishop_Moves.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define make_unique(x) \
+    sort(all((x)));    \
+    (x).erase(unique(all((x))), (x).end())
+void marwan() {
+    int a, b, x, y;
+    cin >> a >> b >> x >> y;
+    if ((a + b) % 2 != (x + y) % 2) {
+        cout << -1 << endl;
+    } else if (abs(a - x) == abs(b - y)) {
+        cout << 1 << endl;
+    } else {
+        cout << 2 << endl;
+    }
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        marwan();
+    }
+    return 0;
+}
