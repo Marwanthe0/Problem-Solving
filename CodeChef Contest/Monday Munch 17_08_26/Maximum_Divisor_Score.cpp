340f20f108f22bf53e5f76bef2eb420f3ae62700
Marwanthe0 <marwanthe0@gmail.com>
2026-08-17 19:48:16 +0600

    Add Maximum_Divisor_Score.cpp

---
+++ CodeChef Contest/Monday Munch 17_08_26/Maximum_Divisor_Score.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define make_unique(x) sort(all((x))); (x).erase(unique(all((x))), (x).end())
+int n;
+vector<int> v,next;
+void marwan()
+{
+        cin >> n;
+        v.assign(n,0);
+        for (auto &vl : v)
+            cin >> vl;
+        int ans = f(0);
+        for(int i = n - 1;i >)
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    marwan();
+    return 0;
+}
