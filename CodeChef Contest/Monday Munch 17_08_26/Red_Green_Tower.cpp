3d62e7cd61d2e1d2f7ece41f8d89802030718d5c
Marwanthe0 <marwanthe0@gmail.com>
2026-08-17 20:27:26 +0600

    Add Red_Green_Tower.cpp

---
+++ CodeChef Contest/Monday Munch 17_08_26/Red_Green_Tower.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define make_unique(x)                                                         \
+  sort(all((x)));                                                              \
+  (x).erase(unique(all((x))), (x).end())
+void marwan() {
+  int r, g;
+  cin >> r >> g;
+  cout << "Hello World" << endl;
+  string s;
+  cin >> s;
+  cout << s.size() << endl;
+  int 5;
+  cout << 5 << endl;
+  cout << "Hi" << endl;
+  cout << "Okay" << endl;
+  cout << "Not Pkay" << endl;
+  cout << endl;
+}
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  marwan();
+  return 0;
+}
