fbcc72443c4cdcf9f5e3af4876e257f3846907ca
Marwanthe0 <marwanthe0@gmail.com>
2026-08-19 20:32:21 +0600

    Add Odd_Even.cpp

---
+++ CodeChef Contest/19_08_26/Odd_Even.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define make_unique(x)                                                         \
+  sort(all((x)));                                                              \
+  (x).erase(unique(all((x))), (x).end())
+void marwan() {
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  int eve = 0, odd = 0;
+  for (auto &vl : v) {
+    cin >> vl;
+    if (vl & 1)
+      odd++;
+    else
+      eve++;
+  }
+  if (odd == eve)
+    cout << odd * 2 << endl;
+  else
+    cout << min(odd, eve) * 2 + 1 << endl;
+}
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    marwan();
+  }
+  return 0;
+}
