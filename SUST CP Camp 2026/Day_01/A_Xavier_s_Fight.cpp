5644529c60647aa1ceb222be70592567aa5b38f4
Marwanthe0 <marwanthe0@gmail.com>
2026-08-22 15:28:22 +0600

    Add A_Xavier_s_Fight.cpp

---
+++ SUST CP Camp 2026/Day_01/A_Xavier_s_Fight.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1e6
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define make_unique(x)                                                         \
+  sort(all((x)));                                                              \
+  (x).erase(unique(all((x))), (x).end())
+void marwan() {
+  int n;
+  cin >> n;
+  cout << fixed << setprecision(3)
+       << ((long double)(n * 1ll * (n + 1))) / (long double)2 << endl;
+}
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    marwan();
+  }
+  return 0;
+}
