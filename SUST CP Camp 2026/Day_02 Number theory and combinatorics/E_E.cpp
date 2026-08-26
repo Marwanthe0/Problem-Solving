495e2ca649da4cfc8f13f73c66034f6479756d27
Marwanthe0 <marwanthe0@gmail.com>
2026-08-26 23:26:45 +0600

    Add E_E.cpp

---
+++ SUST CP Camp 2026/Day_02 Number theory and combinatorics/E_E.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000005
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define make_unique(x)                                                         \
+  sort(all((x)));                                                              \
+  (x).erase(unique(all((x))), (x).end())
+void marwan() {
+  while (1) {
+    int x;
+    cin >> x;
+    if (!x)
+      break;
+    int sum = 0;
+    for (int i = 1; i <= x; i++) {
+      int rem = x - i;
+      sum += 
+    }
+  }
+}
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  marwan();
+  return 0;
+}
