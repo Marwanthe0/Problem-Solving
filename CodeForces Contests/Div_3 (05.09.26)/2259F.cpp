378026e1bfbda1d54c97e21d9d57314aa9cdcb0e
Marwanthe0 <marwanthe0@gmail.com>
2026-09-05 22:08:23 +0600

    Add 2259F.cpp

---
+++ CodeForces Contests/Div_3 (05.09.26)/2259F.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000005
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define make_unique(x)                                                         \
+  sort(all((x)));                                                              \
+  (x).erase(unique(all((x))), (x).end())
+void marwan() {
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  for (auto &vl : v)
+    cin >> vl;
+  string s;
+  cin >> s;
+  int o = 0, ans = 0;
+  bool flag = 0;
+  for (int i = 0; i < n; i++) {
+    if (v[i])
+      o++;
+    else
+      ans += o;
+  }
+  cout << ans << endl;
+  for (int i = 0; i < n; i++) {
+    if (s[i] == '0') {
+      
+    }
+  }
+}
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    marwan();
+  }
+  return 0;
+}
