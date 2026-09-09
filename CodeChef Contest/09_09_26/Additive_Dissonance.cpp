84d23c465bd0edd23443e6ce4f097d7792e06c1f
Marwanthe0 <marwanthe0@gmail.com>
2026-09-09 21:01:42 +0600

    Add Additive_Dissonance.cpp

---
+++ CodeChef Contest/09_09_26/Additive_Dissonance.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define M 1000000007
+#define N 1000005
+#define INF 1e17
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define minus cout << "-1" << endl
+#define zero cout << "0" << endl
+#define make_unique(x)                                                         \
+  sort(all((x)));                                                              \
+  (x).erase(unique(all((x))), (x).end())
+void marwan() {
+  int n;
+  cin >> n;
+  map<int, int> m;
+  vector<int> v(n), t;
+  for (auto &vl : v) {
+    cin >> vl;
+    m[vl]++;
+  }
+  for (auto [x, y] : m)
+    t.push_back(y);
+  sort(all(t));
+  reverse(all(t));
+  cout << (t[0] + 1) / 2 << endl;
+}
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    marwan();
+  }
+  return 0;
+}
