e88d49056a2a5970673d60dda758e61ef9709d7e
Marwanthe0 <marwanthe0@gmail.com>
2026-09-16 14:43:38 +0600

    Update 106642K.cpp

--- Team_contest/Day_02/106642K.cpp
+++ Team_contest/Day_02/106642K.cpp
@@ -10,67 +10,20 @@ using namespace std;
 #define no cout << "No" << endl
 #define minus cout << "-1" << endl
 #define zero cout << "0" << endl
-#define make_unique(x)                                                         \
-  sort(all((x)));                                                              \
-  (x).erase(unique(all((x))), (x).end())
-void marwan() {
-  int n, m;
-  cin >> n >> m;
-  vector<vector<int>> v(n, vector<int>(m, 0));
-  vector<string> t(n);
-  vector<int> row(n, 0), cols(m, 0), rw(n, 0), cls(m, 0);
-  for (int i = 0; i < n; i++) {
-    cin >> t[i];
-    for (int j = 0; j < m; j++) {
-      v[i][j] = t[i][j] - '0';
-      cls[j] += v[i][j];
-      rw[i] += v[i][j];
-    }
-  }
-  for (int i = 0; i < n; i++) {
-    int count = 0;
-    for (int j = 0; j < m; j++) {
-      if (v[i][j])
-        count += cls[j] - 1;
-    }
-    row[i] = count;
-  }
-  for (int j = 0; j < m; j++) {
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-      if (v[i][j])
-        count += rw[i] - 1;
-    }
-    cols[j] = count;
-  }
-  //   for (auto vl : row)
-  //     cout << vl << " ";
-  //   cout << endl;
-  //   for (auto vl : cols)
-  //     cout << vl << " ";
-  //   cout << endl;
-  //   cout << endl;
-  map<int, int> ans;
-  int sum = accumulate(all(row), 0ll) + accumulate(all(cols), 0ll);
-  cerr << sum << endl;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      if (!v[i][j]) {
-        int x = sum - (row[i] + cols[j]);
-        x += rw[i] * cls[j];
-        ans[x]++;
-      }
-    }
-  }
-  cout << (--ans.end())->first << " " << (--ans.end())->second << endl;
+#define make_unique(x) sort(all((x))); (x).erase(unique(all((x))), (x).end())
+void marwan()
+{
+
 }
-int32_t main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  int t;
-  cin >> t;
-  while (t--) {
-    marwan();
-  }
-  return 0;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marwan();
+    }
+    return 0;
 }
